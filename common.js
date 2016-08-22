9c9a2b7c6485bf43dcc163fc4f6c811d8a628d12
SensitMarc <sensitlabs@gmail.com>
2016-08-21 22:34:16 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,3 @@
 // Init UserApp with your App Id
 // https://help.userapp.io/customer/portal/articles/1322336-how-do-i-find-my-app-id-
-UserApp.initialize({ appId: "YOUR-USERAPP-APP-ID" });
+UserApp.initialize({ appId: "57b349611262d" });
